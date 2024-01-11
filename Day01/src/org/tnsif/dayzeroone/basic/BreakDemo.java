893cc9c32d686fee082589a4e6f075ef6c72e363
uspoo <uspoo@MYPC>
2024-01-11 10:26:07 +0530

    first commit

---
+++ Day01/src/org/tnsif/dayzeroone/basic/BreakDemo.java
@@ -0,0 +1,10 @@
+package org.tnsif.dayzeroone.basic;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !!");
+	}
+
+}
